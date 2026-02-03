4f2ff2096d9cbc653cf1c66df9d96661149006f1
Romy Aboudarham <63166855+romyaboudarham@users.noreply.github.com>
2026-02-03 14:38:48 -0800

    linked figma tokens to tailwind!

--- curiosity-cards-app/app/layout.tsx
+++ curiosity-cards-app/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-bg text-fg" suppressHydrationWarning>
+      <body className="bg-surface-background text-text-body" suppressHydrationWarning>
         <main>{children}</main>
       </body>
     </html>
--- curiosity-cards-app/app/page.tsx
+++ curiosity-cards-app/app/page.tsx
@@ -1,30 +1,38 @@
-"use client"
+'use client';
 
 import { useState, useEffect } from 'react';
 
-const tickerWords = ["study", "learn", "make"];
+const tickerWords = ['study', 'learn', 'make'];
 
 export default function Home() {
-    const [tickerIndex, setTickerIndex] = useState(0);
+  const [tickerIndex, setTickerIndex] = useState(0);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setTickerIndex(i => (i + 1) % tickerWords.length);
-        }, 500);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTickerIndex((i) => (i + 1) % tickerWords.length);
+    }, 500);
 
-        return () => clearInterval(interval); // cleanup function, avoid memory leaks
-    }, []);
+    return () => clearInterval(interval); // cleanup function, avoid memory leaks
+  }, []);
 
-    return (
-        <div className="flex min-h-screen flex-col items-center justify-center">
-            <h1 className="text-4xl font-bold text-center">
-                <span className="block">Curious?</span>
-                <span className="inline-block w-20 text-right text-primary">{tickerWords[tickerIndex]}</span>
-                <span className="ml-6">it.</span>
-            </h1>
-            <h2>Generate flashcards on any topic instantly</h2>
-            <input className="border-2 border-gray-300 rounded-md p-2" type="text" placeholder={'Try "types of dinosaurs"'} />
-            <button>Generate Flashcards</button>
-        </div>
-    );
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-center">
+        <span className="block">Curious?</span>
+        <span className="inline-block w-20 text-right text-text-body-brand">
+          {tickerWords[tickerIndex]}
+        </span>
+        <span className="ml-6">it.</span>
+      </h1>
+      <h2>Generate flashcards on any topic instantly</h2>
+      <input
+        className="border-1 border-border rounded-md p-2 focus:outline-none focus:border-border-focus"
+        type="text"
+        placeholder={'Try "types of dinosaurs"'}
+      />
+      <button className="bg-surface-button-button-primary rounded-md p-2 text-text-button-primary">
+        Generate Flashcards
+      </button>
+    </div>
+  );
 }
